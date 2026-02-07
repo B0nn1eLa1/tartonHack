031ad5546c9b924a84b142f848e1de043a73d108
B0nn1eLa1 <163808399+B0nn1eLa1@users.noreply.github.com>
2026-02-07 15:31:55 -0500

    Update BACKEND_URL to use correct variable name

--- frontend/app.py
+++ frontend/app.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta
 # =========================
 # Backend API URL (Ensure FastAPI is running on port 8000)
 # BACKEND_URL = "http://127.0.0.1:8000"
-BACKEND_BASE = "https://tartonhack.onrender.com/"
+BACKEND_URL = "https://tartonhack.onrender.com/"
 
 # Streamlit Page Configuration
 st.set_page_config(
