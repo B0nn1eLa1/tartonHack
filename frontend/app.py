6e18873f5be98a9683ec25eab7015564130100af
B0nn1eLa1 <163808399+B0nn1eLa1@users.noreply.github.com>
2026-02-07 15:47:33 -0500

    Update BACKEND_URL to production URL

--- frontend/app.py
+++ frontend/app.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 # 🔧 Settings & Constants
 # =========================
 # Backend API URL (Ensure FastAPI is running on port 8000)
-BACKEND_URL = "http://127.0.0.1:8000"
+BACKEND_URL = "https://tartonhack.onrender.com/"
 
 # Streamlit Page Configuration
 st.set_page_config(
